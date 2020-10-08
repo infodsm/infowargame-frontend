2ce236c70b348e60da7c30577b9722db695deb50
jsho45n <tpgns20233@gmail.com>
2020-10-08 14:17:28 +0900

    unusbable data delete

--- info-ctf-frontend/src/pages/ShowQuiz.js
+++ info-ctf-frontend/src/pages/ShowQuiz.js
@@ -18,18 +18,12 @@ justify-content: center;
 align-items: center;
 `;
 
-const sampledata = {
-    name: "Test",
-    contents: "HTTP는 웹상에서 클라이언트와 서버 간에 요청/응답으로 데이터를 주고받을 수 있는 프로토콜입니다. 클라이언트가 HTTP 프로토콜을 통해 서버에게 요청을 보내면 서버는 요청에 맞는 응답을 클라이언트에게 전송합니다.",
-    score: "10점"
-};
-
 const ShowQuiz = () => {
 
     return (
         <>
             <ShowQuizBlock>
-                <LoadQuizContainer data={sampledata} />
+                <LoadQuizContainer />
             </ShowQuizBlock>
         </>
     );
