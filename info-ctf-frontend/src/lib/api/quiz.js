58f3d1dde81dfb6c764b1067c77cdcb91e0eb5b6
jsho45n <tpgns20233@gmail.com>
2020-10-15 13:43:14 +0900

    [update]정답체크 API 함수 추가

--- info-ctf-frontend/src/lib/api/quiz.js
+++ info-ctf-frontend/src/lib/api/quiz.js
@@ -10,3 +10,6 @@ export const loadquiz = quiz_code => client.get(`/api/challenge/${quiz_code}`);
 
 // 문제 파일 다운로드
 export const downloadfile = quiz_code => client.get(`/api/challenge/download/${quiz_code}`, { headers: { responseType: 'blob' } });
+
+// 문제 정답 체크
+export const checkanswer = ({ quiz_code, flag, token }) => client.post(`/api/challenge/answer/${quiz_code}`, { flag }, { headers: { "Authentication": token } });
