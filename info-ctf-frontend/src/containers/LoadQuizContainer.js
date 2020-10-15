4a9ea8b67ec096498932d49ae70fe14b722adcff
jsho45n <tpgns20233@gmail.com>
2020-10-15 13:42:53 +0900

    [update]정답체크 API 추가

--- info-ctf-frontend/src/containers/LoadQuizContainer.js
+++ info-ctf-frontend/src/containers/LoadQuizContainer.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import ShowQuizItem from '../components/Quiz/ShowQuizItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { loadquizPost, initialize } from '../modules/loadquiz';
 import { deletequizPost, quizinitialize } from '../modules/deletequiz';
 import { deletefilePost } from '../modules/deletefile';
+import { changeField, checkanswerinitialize, checkanswerPost } from '../modules/checkanswer';
 // import { downloadfilePost, downloadinitialize } from '../modules/downloadfile';
 // import { saveAs } from 'file-saver';
 
@@ -16,15 +17,21 @@ const LoadQuizContainer = ({ match, history }) => {
     const { num } = match.params;
     var quiz_num = { num }.num;
     // const fileSaver = require('file-saver');
-    const { loadquiz, deletequiz, deletequizerror, error, loading } = useSelector(({ loadquiz, deletequiz, deletefile, downloadfile, loading }) => ({
+    const { loadquiz, deletequiz, deletequizerror, checkanswer, checkanswererror, flag, error, loading } = useSelector(({ loadquiz, deletequiz, deletefile, downloadfile, loading, checkanswer }) => ({
         loadquiz: loadquiz.loadquiz,
         error: loadquiz.error,
         deletequiz: deletequiz.deletequiz,
         deletequizerror: deletequiz.error,
         downloadfile: downloadfile.downloadfile,
         downloadfileerror: downloadfile.error,
+        checkanswer: checkanswer.checkanswer,
+        checkanswererror: checkanswer.error,
+        flag: checkanswer.flag,
         loading: loading['loadquiz/LOAD_QUIZ'],
     }));
+
+    // 인풋 값 업데이트
+    const onChange = useCallback(payload => dispatch(changeField(payload)), [dispatch]);
 
     // 페이지가 마운트(처음 보여질 때)될 때 퀴즈목록 api 요청
     useEffect(() => {
@@ -32,6 +39,7 @@ const LoadQuizContainer = ({ match, history }) => {
 
         return () => {
             dispatch(initialize());
+            dispatch(checkanswerinitialize());
         };
     }, [dispatch, num]);
 
@@ -83,6 +91,19 @@ const LoadQuizContainer = ({ match, history }) => {
         */
     };
 
+    // 문제 정답 체크
+    const onCheckAnswer = (quiz_code) => {
+        const users = localStorage.getItem("users") ? localStorage.getItem('user') : null;
+        const admin = localStorage.getItem("admin") ? localStorage.getItem('admin') : null;
+        const token = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
+        if (admin) {
+            dispatch(checkanswerPost({ quiz_code, flag, token }));
+        }
+        if (users) {
+            dispatch(checkanswerPost({ quiz_code, flag, token }));
+        }
+    };
+
     useEffect(() => {
         if (loadquiz) {
         }
@@ -101,6 +122,17 @@ const LoadQuizContainer = ({ match, history }) => {
             console.log(deletequizerror);
         }
     }, [deletequiz, deletequizerror, history]);
+
+    useEffect(() => {
+        if (checkanswer) {
+            alert("정답입니다!");
+            history.push("/challenges");
+        }
+        if (checkanswererror) {
+            alert("오답입니다.");
+            window.location.reload();
+        }
+    }, [checkanswer, checkanswererror, history]);
 
     // 리덕스코드로 파일다운로드 처리가 잘 되지 않아 fetch로 수정, 다운로드가 되므로 리덕스 코드 주석 처리
     /*useEffect(() => {
@@ -121,7 +153,7 @@ const LoadQuizContainer = ({ match, history }) => {
         }
     }, [downloadfile, downloadfileerror]);*/
 
-    return <ShowQuizItem loadquiz={loadquiz} loading={loading} onSubmit={onSubmit} onDownload={onDownload} quiz_code={quiz_num} />;
+    return <ShowQuizItem loadquiz={loadquiz} loading={loading} flag={flag} onChangeField={onChange} onSubmit={onSubmit} onDownload={onDownload} quiz_code={quiz_num} onCheckAnswer={onCheckAnswer} />;
 };
 
 
