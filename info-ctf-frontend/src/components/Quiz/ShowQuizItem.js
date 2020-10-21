34736003f23b57dd8c28f3ea9a794f34ce9fb48e
jsho45n <tpgns20233@gmail.com>
2020-10-21 10:29:24 +0900

    기능구현완료

--- info-ctf-frontend/src/components/Quiz/ShowQuizItem.js
+++ info-ctf-frontend/src/components/Quiz/ShowQuizItem.js
@@ -179,7 +179,7 @@ const ShowQuizItem = ({ loadquiz, flag, loading, onChangeField, onSubmit, onDown
         <>
             <Header />
             <ShowQuizitemBox>
-                <p style={{ position: 'relative', fontSize: '18px', color: 'white', right: '350px', bottom: '220px' }}>*유의사항* : 파일은 모두 zip 형식으로 다운로드됩니다. </p>
+                <p style={{ position: 'relative', fontSize: '18px', color: 'white', right: '270px', bottom: '220px' }}>*유의사항* : 파일은 모두 zip 형식으로 다운로드됩니다. <br /> *유의사항* 파일 용량이 클 경우 다운로드에 시간이 걸릴 수도 있습니다.</p>
                 {admin ? <StyledButton style={{ border: 'none', color: 'red', top: '200px', borderBottom: 'none', zIndex: '1' }} onClick={onSubmit}>문제삭제</StyledButton> : null}
                 <QuizContent style={{ whiteSpace: 'pre' }}>
                     <hr style={{ width: "1125px", marginRight: '150px', marginTop: '100px' }} />
